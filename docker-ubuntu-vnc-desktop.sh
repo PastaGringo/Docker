90e6508c88d59ee6e62b81d529d9683478fbe153
PastaGringo <cestdelafoudre@gmail.com>
2019-01-02 11:56:34 +0100

    Update docker-ubuntu-vnc-desktop.sh

--- docker-ubuntu-vnc-desktop.sh
+++ docker-ubuntu-vnc-desktop.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+echo "Installation des pré-requis docker..."
+apt-get update && sudo apt-get -y install apt-transport-https python3-pip ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+echo "Installation de docker-ce..."
+apt-get install docker-ce
+echo "Installation docker-compose..."
+pip3 install docker-compose
+echo "Création de l'utilisateur docker..."
+useradd -m dockeruser
+passwd dockeruser
+usermod -aG dockeruser sudo docker
+cd /home/dockeruser
+su dockeruser
 echo "Création du certificat SSL..."
 mkdir -p ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ssl/nginx.key -out ssl/nginx.crt
