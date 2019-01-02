24d4f2b5013a58d4fb4f030271f541be2f5a2daa
PastaGringo <cestdelafoudre@gmail.com>
2019-01-02 11:57:53 +0100

    Update docker-ubuntu-vnc-desktop.sh

--- docker-ubuntu-vnc-desktop.sh
+++ docker-ubuntu-vnc-desktop.sh
@@ -18,4 +18,4 @@ echo "Création du certificat SSL..."
 mkdir -p ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ssl/nginx.key -out ssl/nginx.crt
 echo "Creation du conteneur..."
-docker run -p 6081:443 -e HTTP_PASSWORD=YourPassword2019 -e SSL_PORT=443 -v ${PWD}/ssl:/etc/nginx/ssl dorowu/ubuntu-desktop-lxde-vnc
+sudo docker run -p 6081:443 -e HTTP_PASSWORD=YourPassword2019 -e SSL_PORT=443 -v ${PWD}/ssl:/etc/nginx/ssl dorowu/ubuntu-desktop-lxde-vnc
